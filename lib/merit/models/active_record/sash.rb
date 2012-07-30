3e720a322cbbb52506d2feaeb9f8343f5a2952f6
Tomasz Gieniusz <tomasz.gieniusz@gmail.com>
2012-07-30 19:58:39 +0200

    added id to badges_sashes table and replaced some SQL queries with AR

--- lib/merit/models/active_record/sash.rb
+++ lib/merit/models/active_record/sash.rb
@@ -1,25 +1,21 @@
 class Sash < ActiveRecord::Base
   has_many :badges_sashes
 
+  def badges
+    badge_ids.collect { |b_id| Badge.find(b_id) }
+  end
+
   def badge_ids
     badges_sashes.collect(&:badge_id)
   end
 
   def add_badge(badge_id)
     bs = BadgesSash.new
-    bs.sash_id = self.id
     bs.badge_id = badge_id
-    bs.save
+    badges_sashes << bs
   end
+
   def rm_badge(badge_id)
-    badges_sashes = BadgesSash.where(:badge_id => badge_id, :sash_id => self.id)
-    # ActiveRecord::Relation#delete|destroy(_all) doesn't work with composite keys.
-    # Badge is not AR model (Ambry) so can't do self.badges.find(badge_id).delete
-    badges_sash = badges_sashes.first
-    badges_sashes.delete_all
-    # delete doesn't run callbacks, do it by hand
-    if Object.const_defined?('BadgesSashObserver') && badges_sash.present?
-      BadgesSashObserver.instance.after_delete(badges_sash)
-    end
+    badges_sashes.where(:badge_id => badge_id).destroy_all
   end
 end
