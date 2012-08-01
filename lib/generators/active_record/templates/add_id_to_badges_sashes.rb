5a8d759fbad0c567f5570cb1f55d405e2429c501
Tomasz Gieniusz <tomasz.gieniusz@gmail.com>
2012-08-01 20:14:44 +0200

    upgrade notes and gem post_install_message

--- lib/generators/active_record/templates/add_id_to_badges_sashes.rb
+++
@@ -1,9 +0,0 @@
-class AddIdToBadgesSashes < ActiveRecord::Migration
-  def self.up
-    add_column :badges_sashes, :id, :primary_key
-  end
-
-  def self.down
-    remove_column :badges_sashes, :id
-  end
-end
--- lib/generators/merit/upgrade_generator.rb
+++
@@ -1,8 +0,0 @@
-module Merit
-  module Generators
-    class UpgradeGenerator < Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
-      hook_for :orm
-    end
-  end
-end
