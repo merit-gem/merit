6e73af61ff30e2e61821ea8877b459ec2924fdb7
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2013-09-23 23:05:59 -0300

    Don't define attr_accessible if using strong_parameters

--- lib/merit/models/active_record/merit/action.rb
+++ lib/merit/models/active_record/merit/action.rb
@@ -4,7 +4,7 @@ module Merit
 
     has_many :activity_logs, class_name: Merit::ActivityLog
 
-    if Rails.version < '4'
+    unless defined?(ActionController::StrongParameters)
       attr_accessible :user_id, :action_method, :action_value, :had_errors,
         :target_model, :target_id, :processed, :log
     end
