7c6b8d6a5e46149cb28bae6a7e1008229d7cb58b
Harley Trung <harley@socialsci.com>
2013-11-02 23:58:11 -0400

    Assume model to be 'Model' when controller_path is 'api/models'

--- lib/merit/base_target_finder.rb
+++ lib/merit/base_target_finder.rb
@@ -10,13 +10,16 @@ module Merit
       @action = action
     end
 
+    def target_model_without_prefix
+      @action.target_model.split('/').last
+    end
+
     def find
-      klass_name = (@rule.model_name || @action.target_model).singularize
+      klass_name = (@rule.model_name || target_model_without_prefix).singularize
       klass = klass_name.camelize.constantize
       klass.find_by_id @action.target_id
     rescue => e
       Rails.logger.warn "[merit] no target found: #{e}. #{caller.first}"
     end
-
   end
 end
