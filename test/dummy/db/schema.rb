eb353e9db624b3904a07d1bed155f44d1ed3367d
Tute Costa <tutecosta@gmail.com>
2014-01-13 12:09:08 -0300

    Fix dummy app schema.rb file

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130329222238) do
+ActiveRecord::Schema.define(:version => 20130329224410) do
 
   create_table "badges_sashes", :force => true do |t|
     t.integer  "badge_id"
