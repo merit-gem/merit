876c37cd8caab8278019ff0f8036e21c90e015fa
Billy <billyconnect@gmail.com>
2014-01-23 21:29:13 +0800

    observers got notified

--- lib/merit/observer.rb
+++ lib/merit/observer.rb
@@ -1,13 +1,28 @@
 module Merit
-  # TODO: Observer is a misleading name, there's no way to register other
-  # observers yet
+  # Observers can be registered in initalizer/merit.rb
+  # Define observers class or module in an Array as String.
+  # The observer must respond to class/module method `process_award(log)`.
+  # The argument log is the log record for assigning points or badge
+  # config.awarding_observers = ["Foo", "Bar"]
   module Observer
     def notify_observers(action_id, related_change, description = '')
       ActivityLog.create(
         action_id: action_id,
         related_change: related_change,
         description: description
-      )
+      ).tap do |log|
+        send_notification(log) if Merit.awarding_observers.present?
+      end
     end
+
+    private
+
+    def send_notification(log)
+      Merit.awarding_observers.each do |observer|
+        observer = observer.constantize
+        observer.send(:process_award, log)
+      end
+    end
+
   end
 end
