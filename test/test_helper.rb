32663fd7bff478258b8f3d8131d5157405f8630b
Arugin <valera-sochi@bk.ru>
2014-03-12 22:22:00 +0400

    This reverts commit c7fe19780b4049010d4d8a80cb4b59240fcf34fd.
    When
    https://github.com/tute/merit/pull/146/files#diff-b36af4be06e39d478b3d39d216a368faR73
    runs configuring eager_load_paths, Merit.orm is always the default
    :active_record. If we move that line to a later initializer, array is
    frozen and can't be modified (adding Mongoid models for instance).
    
    Conflicts:
        lib/merit.rb
        test/test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -38,20 +38,5 @@ end
 
 require "orm/#{Merit.orm}"
 
-# TODO: tests loads active record and mongoid models in one model
-# from lib folder and mix methods from two orms. It is ugly hack,
-# but I can't find another solution. Need advise.
-if mongoid_orm?
-  Merit.send(:remove_const, :ActivityLog)
-  Merit.send(:remove_const, :BadgesSash)
-  Merit.send(:remove_const, :Sash)
-  Merit.send(:remove_const, :Score)
-end
-
-require "merit/models/#{Merit.orm}/merit/activity_log"
-require "merit/models/#{Merit.orm}/merit/badges_sash"
-require "merit/models/#{Merit.orm}/merit/sash"
-require "merit/models/#{Merit.orm}/merit/score"
-
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
