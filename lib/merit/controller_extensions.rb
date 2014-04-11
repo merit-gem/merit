6d82f783b6e6a55c76a85b92d991194377d3affb
Tute Costa <tutecosta@gmail.com>
2014-04-11 11:19:10 -0400

    Better warning message.
    When using `model_name` option in the rule, controller doesn't find its
    `target_object` and that is ok (reputation is still granted after).
    
    Warning is still useful for newcomers who are not yet familiar with the
    option.
    
    [fixes #154]

--- lib/merit/controller_extensions.rb
+++ lib/merit/controller_extensions.rb
@@ -36,9 +36,10 @@ module Merit
     def target_object
       target_obj = instance_variable_get(:"@#{controller_name.singularize}")
       if target_obj.nil?
-        str = '[merit] No object found, maybe you need a ' +
+        str = '[merit] No object found, you might need a ' +
           "'@#{controller_name.singularize}' variable in " +
-          "'#{controller_path}_controller'?"
+          "'#{controller_path}_controller' if no reputation is applied. " +
+          'If you are using `model_name` option in the rule this is ok.'
         Rails.logger.warn str
       end
       target_obj
