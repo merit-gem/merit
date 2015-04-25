c2eca44b76f3b09b153e1a6d446def0c753c1415
Tute Costa <tutecosta@gmail.com>
2015-04-24 23:13:30 -0400

    Add validations to BadgesSash

--- lib/merit/models/active_record/merit/badges_sash.rb
+++ lib/merit/models/active_record/merit/badges_sash.rb
@@ -1,9 +1,12 @@
 module Merit
   class BadgesSash < ActiveRecord::Base
     include Base::BadgesSash
+
     has_many :activity_logs,
              class_name: 'Merit::ActivityLog',
              as: :related_change
+
+    validates_presence_of :badge_id, :sash
 
     attr_accessible :badge_id if show_attr_accessible?
   end
