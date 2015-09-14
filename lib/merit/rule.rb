de251bfea957072b293ca8c2b8afe0152339c789
Federico Moretti <federico.moretti@woopasoft.com>
2015-09-14 10:44:07 -0300

    Added ability to call badge block only with current user

--- lib/merit/rule.rb
+++ lib/merit/rule.rb
@@ -12,13 +12,10 @@ module Merit
       if block.arity == 0
         block.call
       else
-        # NOTE: For index actions, where there's no target_obj, it may make
-        # sense to grant reputation based only on current_user? That case is
-        # not allowed with this conditional. API feels still weird.
-        if target_obj.present?
-          call_block_with_args(target_obj, current_user)
+        if target_obj.present? || current_user.present?
+          block.call(target_obj, current_user: current_user)
         else
-          Rails.logger.warn '[merit] no target_obj found on Rule#applies?'
+          Rails.logger.warn '[merit] no target_obj or current_user found on Rule#applies?'
           false
         end
       end
@@ -27,21 +24,6 @@ module Merit
     # Get rule's related Badge.
     def badge
       @badge ||= Badge.find_by_name_and_level(badge_name, level)
-    end
-
-    private
-
-    def call_block_with_args(target_obj, current_user)
-      if block.arity == 1
-        block.call(target_obj)
-      elsif block.arity == 2
-        if current_user.present?
-          block.call(target_obj, current_user)
-        else
-          Rails.logger.warn '[merit] no current_user found on Rule#applies?'
-          false
-        end
-      end
     end
 
   end
